1ea4590a4ae2a889344ab1079b73cc727cbe181e
Hee611 <th0611@kakao.com>
2023-08-01 17:58:21 +0900

    ✅ 과제 2023.08.01 수정

--- Academy/230731_cs/Program.cs
+++ Academy/230731_cs/Program.cs
@@ -76,33 +76,38 @@ namespace LearnCS_Collections
                         }
                     }
                 } else if (menuNum == 4) { // 종료 + 먹을 메뉴 선택
-                    int choice = 0, totalMenu = 0, totalPrice = 0;
+                    int choice = 0, totalMenu = 0, totalPrice = 0, length = 0;
 
-                    if (choice == 4) {
-                        Console.WriteLine("{0}개의 메뉴를 선택하셨습니다.", totalMenu);
+                    while (true) {
+                        for (int i = 0; i < menuDic.Count; i++) {
+                            if (length >= 3) { // 글자수가 3 이상이면 탭을 한번
+                                Console.WriteLine("{0}. {1}\t{2:#,###}", i + 1, menuDic.Keys.ToList()[i], menuDic.Values.ToList()[i]);
+                            } else {            // 글자수가 3 미만이면 앞에 탭을 두번
+                                Console.WriteLine("{0}. {1}\t\t{2:#,###}", i + 1, menuDic.Keys.ToList()[i], menuDic.Values.ToList()[i]);
+                            }
+                        }
+
+                        int exit = menuDic.Count + 1;
+
+                        Console.WriteLine("{0}. 메뉴 선택 종료", exit);
+                        Console.Write("메뉴를 선택하세요 : ");
+
+                        choice = int.Parse(Console.ReadLine());
+
+                        if (choice - 1 == menuDic.Count) {
+                            Console.WriteLine("{0}개의 메뉴를 선택하셨습니다.", totalMenu);
+                            Console.WriteLine("모두 {0:#,###}원 입니다.", totalPrice);
+                            break;
+                        } else {
+                            totalMenu++;
+                            totalPrice += menuDic.Values.ToList()[choice - 1];
+                        }
+                    }
+
+                    if (choice - 1 == menuDic.Count) {
                         break;
                     }
 
-                    while (true) {
-                        int cnt = 0;
-                        foreach (string key in menuDic.Keys) {
-                            int length = key.Length;
-                            if (length >= 3) { // 글자수가 3 이상이면 탭을 한번
-                                Console.WriteLine("{0}. {1}\t{2:#,###}", ++cnt, key, menuDic[key]);
-                            } else {            // 글자수가 3 미만이면 앞에 탭을 두번
-                                Console.WriteLine("{0}. {1}\t\t{2:#,###}", ++cnt, key, menuDic[key]);
-                            }
-                        }
-                        Console.WriteLine("{0}. 메뉴 선택 종료", ++cnt);
-                        Console.WriteLine("메뉴를 선택하세요 : ");
-
-                        choice = int.Parse(Console.ReadLine());
-                        if (choice == menuDic.Count) break;
-                        else {
-                            totalMenu++;
-
-                        }
-                    }
                 } else { // 초기 선택 화면의 번호들이 아닌 경우
                     Console.WriteLine("올바른 값을 입력해 주세요.");
                 }
