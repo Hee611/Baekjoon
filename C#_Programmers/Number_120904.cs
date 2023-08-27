820e5f66d5dbcf6ef25840a6f8eff3d3f3234217
Hee611 <th0611@kakao.com>
2023-08-27 21:59:26 +0900

    ✅ 코딩테스트 연습 - 숫자 찾기

---
+++ C#_Programmers/Number_120904.cs
@@ -0,0 +1,8 @@
+using System;
+
+public class Solution {
+    public int solution(int num, int k) {
+        int answer = num.ToString().IndexOf(k.ToString()) + 1;
+        return answer == 0 ? -1 : answer;
+    }
+}
