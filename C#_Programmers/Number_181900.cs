440692a3914881970d36be078515842a3bfabcaa
Hee611 <th0611@kakao.com>
2023-09-03 23:38:32 +0900

    ✅ 코딩테스트 연습 - 글자 지우기

--- C#_Programmers/Number_181900.cs
+++ C#_Programmers/Number_181900.cs
@@ -3,6 +3,6 @@ using System.Linq;
 
 public class Solution {
     public string solution(string my_string, int[] indices) {
-        return new string(my_string.Where((i, index) => !indices.Contains(index)).ToArray());;
+        return new string(my_string.Where((i, index) => !indices.Contains(index)).ToArray());
     }
 }
