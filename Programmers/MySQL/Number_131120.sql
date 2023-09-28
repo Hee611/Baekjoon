3186ab18c6d2c127b0bcaf1b5b1f2e7bfe692e56
Hee611 <th0611@kakao.com>
2023-09-28 22:18:54 +0900

    ✅ 3월에 태어난 여성 회원 목록 출력하기

---
+++ Programmers/MySQL/Number_131120.sql
@@ -0,0 +1,6 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131120
+
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, "%Y-%m-%d") as DATE_OF_BIRTH
+FROM MEMBER_PROFILE
+WHERE DATE_OF_BIRTH like "%-03-%" AND GENDER = "W" AND TLNO IS NOT NULL
+ORDER BY MEMBER_ID ASC;
